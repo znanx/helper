7e3ca4d30dc5ac37079c56a447214e24c3ffce89
rifnd <rifando.p.p@gmail.com>
2025-09-24 11:14:49 +0700

    feat(add): imghost

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import axios from 'axios'
-import { fileTypeFromBuffer } from 'file-type'
+import { fromBuffer as getExtension } from 'file-type'
+import { v4 as uuidv4 } from 'uuid'
+import * as cheerio from 'cheerio'
 import FormData from 'form-data'
+import retry from 'async-retry'
 import path from 'path'
 import helper from './helper'
 const creator = '@naando.io'
@@ -42,7 +45,7 @@ exports.uploader = (i: Buffer | string, originalFilePath = null) => new Promise(
             msg: 'Failed to process file input',
          })
       }
-      let fileType = await fileTypeFromBuffer(file)
+      let fileType = await getExtension(file)
       let fileName = 'file'
       if (originalFilePath) {
          fileName = path.basename(originalFilePath || '', path.extname(originalFilePath || ''))
@@ -76,7 +79,7 @@ exports.tmpfiles = (i: Buffer | string, extension?: string) => new Promise(async
          responseType: 'arraybuffer'
       })).data : null
       let ext = 'txt'
-      const fileTypeResult = await fileTypeFromBuffer(file)
+      const fileTypeResult = await getExtension(file)
       if (fileTypeResult) {
          ext = fileTypeResult?.ext || 'txt'
       }
@@ -139,10 +142,10 @@ exports.imgbb = (i: Buffer | string) => new Promise(async (resolve, reject) => {
          responseType: 'arraybuffer'
       })).data : null
       try {
-         const fileTypeResult = await fileTypeFromBuffer(file)
-         var ext = fileTypeResult ? fileTypeResult.ext : 'jpg'
+         const fileTypeResult = await getExtension(file)
+         var ext: string = fileTypeResult ? fileTypeResult.ext : 'jpg'
       } catch (e) {
-         var ext = 'jpg'
+         var ext: string = 'jpg'
       }
       let form = new FormData
       form.append('source', Buffer.from(file), 'image.' + ext)
@@ -179,6 +182,78 @@ exports.imgbb = (i: Buffer | string) => new Promise(async (resolve, reject) => {
          creator,
          status: false,
          msg: e instanceof Error ? e.message : String(e)
+      })
+   }
+})
+exports.imghost = (i: Buffer | string): Promise<any> => new Promise(async (resolve, reject) => {
+   try {
+      if (!Buffer.isBuffer(i) && !helper.isUrl(i)) throw new Error('Only buffer and url formats are allowed')
+      const parse = await (await axios.get('https://imghost.online/en', {
+         headers: {
+            "User-Agent": "Mozilla/5.0 (Linux; Android 6.0.1; SM-J500G) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Mobile Safari/537.36"
+         }
+      }))
+      const token = cheerio.load(parse.data)('meta[name="csrf-token"]')?.attr('content')
+      const cookie = parse?.headers?.['set-cookie']?.join(';')
+      if (!token || !cookie) throw new Error('Can\'t get credentials')
+      const file = Buffer.isBuffer(i) ? i : helper.isUrl(i) ? await (await axios.get(i, {
+         responseType: 'arraybuffer'
+      })).data : null
+      let ext = 'jpg'
+      const parsed = await getExtension(file)
+      if (parsed) {
+         ext = parsed?.ext || 'jpg'
+      }
+      let form = new FormData
+      form.append('dzuuid', uuidv4())
+      form.append('dzchunkindex', '0')
+      form.append('dztotalfilesize', file.length)
+      form.append('dzchunksize', file.length)
+      form.append('dztotalchunkcount', '1')
+      form.append('dzchunkbyteoffset', '0')
+      form.append('size', file.length)
+      form.append('type', parsed?.mime)
+      form.append('password', '')
+      form.append('upload_auto_delete', '0')
+      form.append('file', Buffer.from(file), 'image.' + ext)
+      const json = await retry(async () => {
+         const response = await (await axios.post('https://imghost.online/upload', form, {
+            headers: {
+               "Accept": "*/*",
+               "User-Agent": "Mozilla/5.0 (Linux; Android 6.0.1; SM-J500G) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Mobile Safari/537.36",
+               "Origin": "https://imghost.online",
+               "Referer": "https://imghost.online/",
+               "Referrer-Policy": "strict-origin-when-cross-origin",
+               cookie,
+               "X-CSRF-TOKEN": token,
+               "X-Requested-With": "XMLHttpRequest",
+               ...form.getHeaders()
+            }
+         })).data
+         if (!response.direct_link) throw new Error('Failed to Upload!')
+         return response
+      }, {
+         retries: 5,
+         factor: 2,
+         minTimeout: 1000,
+         maxTimeout: 5000,
+         onRetry: (e, n) => { }
+      })
+
+      if (!json.direct_link) throw new Error('Failed to Upload!')
+      resolve({
+         creator,
+         status: true,
+         original: json,
+         data: {
+            url: json.direct_link
+         }
+      })
+   } catch (e) {
+      resolve({
+         creator,
+         status: false,
+         msg: e.message
       })
    }
 })
